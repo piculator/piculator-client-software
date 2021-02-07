fbce93bc733718c9424fe2fe0677e2d2b6e8ad07
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-07 11:52:45 +0800

    modify tile size.

--- src/app/ui/controls/tile_factory.py
+++ src/app/ui/controls/tile_factory.py
@@ -3,9 +3,8 @@ from PyQt5.QtWidgets import QPushButton
 
 def make_tile(imgpath: str, text: str, action) -> QPushButton:
     btn = QPushButton()
-    size = 80
-    btn.setFixedWidth(size)
-    btn.setFixedHeight(size)
+    btn.setFixedWidth(120)
+    btn.setFixedHeight(100)
     btn.setText(text)
     btn.setStyleSheet('QPushButton {background-image: url("'+imgpath+f'");background-position: center; background-repeat: no-repeat;}}')
     btn.clicked.connect(action)
