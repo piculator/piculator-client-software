d3a77a21b7f7d6ada5c8f667d058db4cb5ac3845
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-18 19:22:30 +0800

    Finish functionality of sage console.

--- src/app/logic/func_sage_console.py
+++ src/app/logic/func_sage_console.py
@@ -1,2 +1,5 @@
+from app import terminal_executable,terminal_execute_parameter,sage_executable
+
 def execute():
-    pass
+    from subprocess import Popen
+    Popen([terminal_executable,terminal_execute_parameter,sage_executable])
