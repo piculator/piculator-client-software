939c2c9a7d7fc52027e0591baa2291593241ab27
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-18 19:17:38 +0800

    Add more constants and modified logic of getting worker_port

--- src/app/constants.py
+++ src/app/constants.py
@@ -6,10 +6,20 @@ class Ports:
     jupyter_notebook = 8888
     # worker 端口区间起始
     worker_range_start = 22222
-    # worker 端口区间终止
-    worker_range_end = 22333
     # 数据桥梁端口
     data_bridge = 40404
 
+    _next_worker_port = worker_range_start
 
-sage_python_interpreter = r"D:\Non-Green\SageMath 9.2\runtime\bin\python3.7m.exe" if platform.system() == 'Windows' else '/home/pi/sage/sage-9.2/local/bin/python3'
+    @classmethod
+    def get_worker_port(cls):
+        cls._next_worker_port += 1
+        return cls._next_worker_port - 1
+
+
+sage_versions_root = '/home/pi/sage'
+sage_root = sage_versions_root + '/sage-9.2'
+sage_python_interpreter = sage_root + '/local/bin/python3'
+sage_executable = sage_root + '/sage'
+terminal_executable = 'x-terminal-emulator'
+terminal_execute_parameter = '-e'
