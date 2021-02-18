66469d31b2e4b4d0519266003121013821569211
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-18 19:16:46 +0800

    Fix bugs and refactored `commander`

--- src/app/ipc/commander.py
+++ src/app/ipc/commander.py
@@ -1,7 +1,24 @@
 from multiprocessing.connection import Client
 
 
-def send_msg(port, auth_key, msg):
-    address = ('localhost', port)
-    conn = Client(address, authkey=auth_key)
-    conn.send(msg)
+class Commander:
+    def __init__(self,port,auth_key):
+        self.address = ('localhost', port)
+        self.auth_key = auth_key
+        self.connection = None
+        self.connected = False
+
+    def connect(self):
+        try:
+            self.connection=Client(self.address, authkey=bytes(self.auth_key,encoding='utf-8'))
+            self.connected = True
+            return True
+        except:
+            return False
+
+
+    def send(self,msg):
+        self.connection.send(msg)
+
+    def close(self):
+        self.connection.close()
