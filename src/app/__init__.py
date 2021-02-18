802cbfc7a852a0ef6166496b685ae4901f6e30b2
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-18 19:23:36 +0800

    Add display environment variable.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,7 +3,8 @@ from app.auth.login_manager import LoginManager
 from app.constants import *
 from app.constants import Ports as ports
 from app.settings import settings
+import os
 
-
+os.environ['DISPLAY'] = ':0.0'
 login_manager = LoginManager()
 myapp = PCSApp()
