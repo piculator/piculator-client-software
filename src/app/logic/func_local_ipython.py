8d0cc60050f55b0063c310aba52aae320700b251
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-22 10:32:22 +0800

    Implement local ipython.

--- src/app/logic/func_local_ipython.py
+++ src/app/logic/func_local_ipython.py
@@ -1,2 +1,4 @@
 def execute():
-    pass
+    from subprocess import Popen
+    from app import terminal_executable, terminal_execute_parameter
+    Popen([terminal_executable,terminal_execute_parameter,'python3','-m','jupyter_console'])
