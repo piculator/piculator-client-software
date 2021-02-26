55c1c1712eda1da95b72153eb87f5b5262612584
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-26 11:04:50 +0800

    Fix bugs.

--- src/app/logic/func_learn_python.py
+++ src/app/logic/func_learn_python.py
@@ -2,4 +2,5 @@ def execute():
     from app import myapp
     w = myapp.get_new_wild_web_window()
     from PyQt5.QtCore import QUrl
-    w.browser.setUrl(QUrl('/usr/share/doc/python3/html/index.html'))
+    w.browser.setUrl(QUrl.fromLocalFile('/usr/share/doc/python3/html/index.html'))
+    w.showMaximized()
--- src/app/logic/func_learn_sage.py
+++ src/app/logic/func_learn_sage.py
@@ -2,4 +2,5 @@ def execute():
     from app import myapp
     w = myapp.get_new_wild_web_window()
     from PyQt5.QtCore import QUrl
-    w.browser.setUrl(QUrl('/usr/share/doc/piculator/sage/html/en/index.html'))
+    w.browser.setUrl(QUrl.fromLocalFile('/usr/share/doc/piculator/sage/html/en/index.html'))
+    w.showMaximized()
