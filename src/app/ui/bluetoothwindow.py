396f6de74063704d3d636f4d01ddfc7e72d64975
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-26 09:16:24 +0800

    放弃蓝牙功能开发

--- src/app/ui/bluetoothwindow.py
+++ src/app/ui/bluetoothwindow.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QWidget
 
 from app.base_ui.bluetoothwindow_base import Ui_BluetoothSharingWindow
-from bluetooth.ble import DiscoveryService
 from bluetooth import discover_devices
+
+from app.logic.func_fallback import prompt_func_not_implemented
 
 
 class BluetoothWindow(Ui_BluetoothSharingWindow,QWidget):
@@ -14,3 +15,6 @@ class BluetoothWindow(Ui_BluetoothSharingWindow,QWidget):
 
     def scan_devices_and_update(self):
         self.combo_devices.addItems(discover_devices())
+
+    def showEvent(self, a0):
+        prompt_func_not_implemented()
