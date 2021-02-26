4609037f168b5a0e3c5eda6d8a2fda33f4706477
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-26 13:31:09 +0800

    Remove button texts.

--- src/app/ui/controls/tile_factory.py
+++ src/app/ui/controls/tile_factory.py
@@ -5,7 +5,7 @@ def make_tile(imgpath: str, text: str, tooltip: str, action) -> QPushButton:
     btn = QPushButton()
     btn.setFixedWidth(140)
     btn.setFixedHeight(100)
-    btn.setText(text)
+    # btn.setText(text)
     btn.setToolTip(tooltip)
     btn.setWhatsThis(tooltip)
     btn.setStyleSheet('QPushButton {background-image: url("'+imgpath+f'");background-position: center; background-repeat: no-repeat;}}')
